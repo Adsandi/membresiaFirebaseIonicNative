4014d4c0a7fb19ef56d6e8304cd8be59c65c8c53
Adrián Sánchez Díaz <adsandiaz@hotmail.es>
2019-02-07 18:40:31 +0100

    probando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
-import { Firebase } from '@ionic-native/firebase';
+
 import { HomePage } from '../pages/home/home';
+import { Firebase } from '@ionic-native/firebase';
 
 var config = {
   apiKey: "AIzaSyBbQPTdeetT6bteuzaRUMAaaFyJXxlUltc",
@@ -33,6 +35,7 @@ var config = {
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
+    
   ]
 })
 export class AppModule {}
