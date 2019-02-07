0cf4dfac76fc3cf29097a731b12cb5220778d179
juanantoniotorres <juanantoniot10@gmail.com>
2019-02-07 16:49:19 +0100

    conectado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
+import { Firebase } from '@ionic-native/firebase';
 import { HomePage } from '../pages/home/home';
 
+var config = {
+  apiKey: "AIzaSyBbQPTdeetT6bteuzaRUMAaaFyJXxlUltc",
+  authDomain: "pruebanube-8d38c.firebaseapp.com",
+  databaseURL: "https://pruebanube-8d38c.firebaseio.com",
+  projectId: "pruebanube-8d38c",
+  storageBucket: "pruebanube-8d38c.appspot.com",
+  messagingSenderId: "612600142016"
+};
 @NgModule({
   declarations: [
     MyApp,
